6648a4a7b6c13a588c731c6243548b18047cd97c
cfigueiroa <cfigueiroa@gmail.com>
2023-06-01 04:54:06 -0300

    feat: dynamic like show

--- src/components/PostComponent/index.jsx
+++ src/components/PostComponent/index.jsx
@@ -1,34 +1,33 @@
 import { useState } from "react";
-import { AuthorName, Container, ImageContent, LinkContent, PictureAndLikes, PostContainer, PostContent, PostText } from "./styles";
+import { AuthorName, ImageContent, LinkContent, PictureAndLikes, PostContainer, PostContent, PostText } from "./styles";
 import { IoHeartOutline, IoHeartSharp } from "react-icons/io5";
-import { useNavigate } from "react-router";
 
+export default function PostComponent({ post, userId }) {
+  const checkLiked = post.liked_by_user_ids.includes(userId);
 
-export default function PostComponent({ post }) {
+  const [liked] = useState(checkLiked);
 
-    const navigate = useNavigate();
-
-    const [liked, setLiked] = useState(false);
-
-    return (
-        <PostContainer>
-            <PictureAndLikes>
-                <img src={post.picture} alt="" />
-                {liked ? < IoHeartSharp color="white" size="20px" /> : < IoHeartOutline color="white" size="20px" />}
-                <h2>1 like</h2>
-            </PictureAndLikes>
-            <PostContent>
-                <AuthorName>{post.username}</AuthorName>
-                <PostText>{post.content}</PostText>
-                <LinkContent href={post.url} target="_blank">
-                    <div>
-                        <h1>{post.url_title}</h1>
-                        <p>{post.url_description}</p>
-                        <h2>{post.url}</h2>
-                    </div>
-                    <ImageContent src={post.url_picture} alt="" />
-                </LinkContent>
-            </PostContent>
-        </PostContainer>
-    )
+  return (
+    <PostContainer>
+      <PictureAndLikes>
+        <img src={post.picture} alt="" />
+        {liked ? <IoHeartSharp color="white" size="20px" /> : <IoHeartOutline color="white" size="20px" />}
+        <h2>
+          {post.liked_by_user_ids.length} like{post.liked_by_user_ids.length > 1 ? "s" : null}
+        </h2>
+      </PictureAndLikes>
+      <PostContent>
+        <AuthorName>{post.username}</AuthorName>
+        <PostText>{post.content}</PostText>
+        <LinkContent href={post.url} target="_blank">
+          <div>
+            <h1>{post.url_title}</h1>
+            <p>{post.url_description}</p>
+            <h2>{post.url}</h2>
+          </div>
+          <ImageContent src={post.url_picture} alt="" />
+        </LinkContent>
+      </PostContent>
+    </PostContainer>
+  );
 }
--- src/pages/TimelinePage/index.jsx
+++ src/pages/TimelinePage/index.jsx
@@ -1,68 +1,72 @@
 import { useEffect, useState } from "react";
 import PostForm from "../../components/PostForm";
-import { AppContainer, ContentDivider, TimelineContainer, TimelineTitle, TrendingHashtagsContainer, TrendingHashtagsTitle } from "./styles";
+import {
+  AppContainer,
+  ContentDivider,
+  TimelineContainer,
+  TimelineTitle,
+  TrendingHashtagsContainer,
+  TrendingHashtagsTitle,
+} from "./styles";
 import useMyContext from "../../contexts/MyContext.jsx";
-import axios from "axios";
-import { PostContainer } from "../../components/PostComponent/styles";
 import PostComponent from "../../components/PostComponent";
 import TrendingHashtags from "../../components/TrendingHashtags/TrendingHashtags.jsx";
 import { useNavigate } from "react-router-dom";
-import { Navigate } from "react-router";
 import API from "../../config/api";
 
-
 export default function TimelinePage() {
+  const navigate = useNavigate();
+  const { user } = useMyContext();
+  const [userData, setUserData] = useState({});
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const navigate = useNavigate();
-    const { user, setUser } = useMyContext();
-    const [userData, setUserData] = useState({});
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    if (!user) return navigate("/");
 
-    useEffect(() => {
-        if (!user) return navigate("/");
+    const requestUserData = API.buscarUsuario(user);
+    requestUserData
+      .then((res) => {
+        setUserData(res.data);
+      })
+      .catch((err) => {
+        console.log("An error occured while trying to fetch the user data, please refresh the page");
+      });
+    const requestPosts = API.buscarPosts(user);
+    requestPosts
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log("An error occured while trying to fetch the posts, please refresh the page");
+      });
+  }, [loading]);
 
-        const requestUserData = API.buscarUsuario(user)
-        requestUserData.then((res) => {
-            setUserData(res.data);
-        }).catch((err) => {
-            console.log("An error occured while trying to fetch the user data, please refresh the page");
-        })
-        const requesPosts = API.buscarPosts(user);
-        //axios.get(`http://localhost:5000/posts`, config);
-        requesPosts.then((res) => {
-            setPosts(res.data);
-        }).catch((err) => {
-            console.log("An error occured while trying to fetch the posts, please refresh the page");
-        })
-
-    }, [loading]);
-
-    function renderPosts() {
-        if (posts) {
-            if (posts.length === 0) return (<h1>There are no posts yet</h1>)
-            else {
-
-            }
-            return posts.map((post) => { return <PostComponent key={post.id} post={post} /> })
-        } else {
-            return <h1>Loading...</h1>
-        }
+  function renderPosts() {
+    if (posts) {
+      if (posts.length === 0) return <h1>There are no posts yet</h1>;
+      else {
+      }
+      return posts.map((post) => {
+        return <PostComponent key={post.id} post={post} userId={userData.id} />;
+      });
+    } else {
+      return <h1>Loading...</h1>;
     }
+  }
 
-
-    return (
-        <AppContainer>
-            <TimelineContainer>
-                <TimelineTitle>timeline</TimelineTitle>
-                <PostForm userPicture={userData.picture} token={user} loading={loading} setLoading={setLoading} />
-                {renderPosts()}
-            </TimelineContainer>
-            <TrendingHashtagsContainer>
-                <TrendingHashtagsTitle>trending</TrendingHashtagsTitle>
-                <ContentDivider></ContentDivider>
-                <TrendingHashtags />
-            </TrendingHashtagsContainer>
-        </AppContainer>
-    )
+  return (
+    <AppContainer>
+      <TimelineContainer>
+        <TimelineTitle>timeline</TimelineTitle>
+        <PostForm userPicture={userData.picture} token={user} loading={loading} setLoading={setLoading} />
+        {renderPosts()}
+      </TimelineContainer>
+      <TrendingHashtagsContainer>
+        <TrendingHashtagsTitle>trending</TrendingHashtagsTitle>
+        <ContentDivider></ContentDivider>
+        <TrendingHashtags />
+      </TrendingHashtagsContainer>
+    </AppContainer>
+  );
 }
