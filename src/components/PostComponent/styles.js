4030d49326093414c13724a664fe9e161adc49e0
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-06-06 13:24:50 -0300

    feat: input comentario mockup

--- src/components/PostComponent/styles.js
+++ src/components/PostComponent/styles.js
@@ -1,5 +1,63 @@
 import styled from "styled-components";
 
+export const PostOuterContainer = styled.div`
+  width: 100%;
+  height: auto;
+  border-radius: 16px;
+  background-color: #1E1E1E;
+  margin-bottom: 16px;
+
+`;
+
+export const CommentContainer = styled.div`
+  width: 100%;
+  height: auto;
+`;
+
+export const CommentInputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 19px 25px 25px;
+  img{
+    width: 39px;
+    height: 39px;
+    border-radius: 26.5px;
+    margin-right: 14px;
+  }
+  form{
+    background-color: #252525;
+    border-radius: 8px;
+    width: 100%;
+    height: 39px;
+    border: none;
+    display: flex;
+    align-items: center;
+    padding: 0px 15px;
+  }
+  input{
+    width: 100%;
+    height: 17px;
+    background-color: none;
+    border: none;
+    font-family: 'Lato';
+    font-style: italic;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    letter-spacing: 0.05em;
+    color: #575757;
+    background-color: transparent;
+    margin-right: 5px;
+    :focus{
+      outline: none;
+    }
+  }
+
+`;
+
 export const PostContainer = styled.div`
   width: 100%;
   height: auto;
@@ -8,7 +66,6 @@ export const PostContainer = styled.div`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   display: flex;
   box-sizing: border-box;
-  margin-bottom: 16px;
   h1 {
     color: white;
   }
@@ -170,9 +227,9 @@ export const LoadingContainer = styled.div`
 `;
 
 export const tagStyle = {
-    fontWeight: "bold",
-    color: "#FFFFFF",
-    cursor: "pointer",
+  fontWeight: "bold",
+  color: "#FFFFFF",
+  cursor: "pointer",
 };
 
 export const EspacoIcones = styled.div`
