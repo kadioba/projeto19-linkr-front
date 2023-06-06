f93c74b3655cf1ef0d085f182034e296880a2937
cfigueiroa <cfigueiroa@gmail.com>
2023-06-06 10:44:25 -0300

    feat: new posts warning

--- src/pages/TimelinePage/TimelinePage.jsx
+++ src/pages/TimelinePage/TimelinePage.jsx
@@ -13,51 +13,49 @@ import TrendingHashtags from "../../components/TrendingHashtags/TrendingHashtags
 import API from "../../config/api";
 import PostsRenderer from "../../components/PostsRenderer/PostsRenderer";
 import useInterval from "../../hooks/useInterval.jsx";
+import NewPostsButton from "../../components/NewPostsButton/NewPostsButton.jsx";
 
 export default function TimelinePage() {
   const { token } = useMyContext();
   const { user } = useMyContext();
   const [posts, setPosts] = useState(undefined);
   const [loading, setLoading] = useState(false);
-  const [apiCallComplete, setApiCallComplete] = useState(false);
-  const [newPostsCount, setNewPostsCount] = useState(0);
   const [newPosts, setNewPosts] = useState(posts);
+  const [newPostsCount, setNewPostsCount] = useState(0);
 
   useEffect(() => {
-    const requestPosts = API.getPosts(token);
-    requestPosts
-      .then((res) => {
-        setPosts(res.data);
-        setApiCallComplete(true);
-      })
-      .catch((_err) => {
-        alert("An error occurred while trying to fetch the posts, please refresh the page");
-      });
+    const fetchPosts = async () => {
+      try {
+        const { data } = await API.getPosts(token);
+        setPosts(data);
+      } catch (err) {
+        alert("An error occured while trying to fetch the posts, please refresh the page");
+      }
+    };
+
+    fetchPosts();
   }, [loading, token]);
 
-  useInterval(() => {
-    if (apiCallComplete) {
-      API.getPosts(token)
-        .then((res) => {
-          const gotPosts = res.data;
-          const prevPosts = posts;
-          if (gotPosts && prevPosts) {
-            const lastPostId = prevPosts[0].id;
-            const newPostsCounter = gotPosts.filter((post) => post.id > lastPostId).length;
-            if (newPostsCounter > 0) {
-              console.log(newPostsCounter);
-              setNewPosts(gotPosts);
-              setNewPostsCount(newPostsCounter);
-            } else {
-              console.log("No new posts");
-            }
+  useInterval(
+    () => {
+      const fetchNewPosts = async () => {
+        try {
+          const { data } = await API.getPosts(token);
+          const lastPostId = posts[0].id;
+          const newPostsCounter = data.filter((post) => post.id > lastPostId).length;
+          if (newPostsCounter) {
+            setNewPosts(data);
+            setNewPostsCount(newPostsCounter);
           }
-        })
-        .catch((err) => {
-          console.error("Interval API error:", err);
-        });
-    }
-  }, 15000);
+        } catch (err) {
+          console.error("Interval API error");
+        }
+      };
+
+      fetchNewPosts();
+    },
+    posts ? 15000 : null
+  );
 
   const updatePosts = () => {
     setPosts(newPosts);
@@ -68,8 +66,8 @@ export default function TimelinePage() {
     <AppContainer>
       <TimelineContainer>
         <TimelineTitle>timeline</TimelineTitle>
-        {newPostsCount ? <button onClick={updatePosts}>aqui carai</button> : <></>}
         <PostForm user={user} token={token} loading={loading} setLoading={setLoading} setPosts={setPosts} />
+        {newPostsCount ? <NewPostsButton updatePosts={updatePosts} newPostsCount={newPostsCount} /> : <></>}
         <PostsRenderer posts={posts} user={user} setPosts={setPosts} />
       </TimelineContainer>
       <TrendingHashtagsContainer data-test="trending">
