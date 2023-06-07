0f48081cee10d9f4e16ab2233834881d9750d86e
Davi Barci <davibarci0208@gmail.com>
2023-06-07 01:37:58 -0300

    feat: added custom messages for missing posts

--- src/components/PostsRenderer/PostsRenderer.jsx
+++ src/components/PostsRenderer/PostsRenderer.jsx
@@ -1,12 +1,21 @@
 import { LoadingComponent } from "../LoadingComponent/LoadingComponent";
 import PostComponent from "../PostComponent/PostComponent";
+import { useLocation } from "react-router-dom";
 
 function PostsRenderer({ posts, user, setPosts }) {
+  const location = useLocation();
+
   if (!posts || !user) return <LoadingComponent />;
   if (posts.length === 0)
     return (
       <h1 data-test="message" style={{ color: "white" }}>
-        There are no posts yet
+        {
+          location.pathname === "/timeline"
+          ? (Object.keys(user.following).length === 0
+              ? "You don't follow anyone yet. Search for new friends!"
+              : "No posts found from your friends")
+          : "There are no posts yet"
+        }
       </h1>
     );
 
